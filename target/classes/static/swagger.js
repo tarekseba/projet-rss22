f3cd596ce28ad234e4206515f500be0472017434
Ahmadou kassoum <ahmadoukassoum7@gmail.com>
2022-05-01 17:44:44 +0200

    update swagger

--- target/classes/static/swagger.js
+++ target/classes/static/swagger.js
@@ -2,36 +2,37 @@
 
 function showDiv() {
     const targetDiv = document.getElementById("afficher");
-    if (targetDiv.style.display !== "none") {
-        targetDiv.style.display = "none";
-    } else {
+    if (targetDiv.style.display == "none"||targetDiv.style.display=="") {
         targetDiv.style.display = "block";
+    } else {
+        targetDiv.style.display = "none";
     }
 }
 
 function showDivGet() {
     const targetDiv = document.getElementById("afficherContentGet");
-    if (targetDiv.style.display !== "none") {
-        targetDiv.style.display = "none";
-    } else {
+    console.log(targetDiv.style.display)
+    if (targetDiv.style.display == "none"||targetDiv.style.display=="") {
         targetDiv.style.display = "block";
+    } else {
+        targetDiv.style.display = "none";
     }
 }
 
 function showDivDel() {
     const targetDiv = document.getElementById("afficherContentDel");
-    if (targetDiv.style.display !== "none") {
-        targetDiv.style.display = "none";
-    } else {
+    if (targetDiv.style.display == "none"||targetDiv.style.display=="") {
         targetDiv.style.display = "block";
+    } else {
+        targetDiv.style.display = "none";
     }
 }
 
 function showDivPost() {
     const targetDiv = document.getElementById("afficherContentPost");
-    if (targetDiv.style.display !== "none") {
-        targetDiv.style.display = "none";
-    } else {
+    if (targetDiv.style.display == "none"||targetDiv.style.display=="") {
         targetDiv.style.display = "block";
+    } else {
+        targetDiv.style.display = "none";
     }
 }
