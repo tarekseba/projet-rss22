496b22219448321296ba5f05ff349b6c64d91747
Ahmadou kassoum <ahmadoukassoum7@gmail.com>
2022-05-04 17:25:03 +0200

    fix cors origin error

--- src/main/java/univ/rouen/rss/projetrss/ProjetRssApplication.java
+++ src/main/java/univ/rouen/rss/projetrss/ProjetRssApplication.java
@@ -20,7 +20,7 @@ public class ProjetRssApplication {
 	public CorsFilter corsFilter(){
 		CorsConfiguration corsConfiguration=new CorsConfiguration();
 		corsConfiguration.setAllowCredentials(true);
-		corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:4200","http://localhost:4200/ajout"));
+		corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:4200","https://app-84a5e82f-d424-4ee9-be89-d9e3262545ee.cleverapps.io"));
 		corsConfiguration.setAllowedHeaders(Arrays.asList("Origin","Access-Control-Allow-Origin","Content-Type","Accept","Authorization","Origin, Accept",
 				"X-Requested-Width","Access-Control-Request-Method","Access-Control-Request-Headers"));
 		corsConfiguration.setExposedHeaders(Arrays.asList("Origin","Access-Control-Allow-Origin","Content-Type","Accept","Authorization",
